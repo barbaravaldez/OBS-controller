c65b73123db59846c4c6ffd989c97f26d6a88263
Barbara Valdez Mireles <barbara@Barbaras-MacBook-Pro.local>
2025-02-03 15:50:51 -0800

    fix client switch scene

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const http = require('http');
-const WebSocket = require('ws');
-const obs = require('./obsClient');
-const fs = require('fs');
+import http from "http";
+import WebSocket from "ws";
+import { obs } from "./obsClient.js";
+import fs from "fs";
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/') {
-    fs.readFile('index.html', (err, data) => {
+  if (req.method === "GET" && req.url === "/") {
+    fs.readFile("index.html", (err, data) => {
       if (err) {
         res.writeHead(500);
-        res.end('Error loading index.html');
+        res.end("Error loading index.html");
       } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.writeHead(200, { "Content-Type": "text/html" });
         res.end(data);
       }
     });
@@ -19,29 +19,32 @@ const server = http.createServer((req, res) => {
 
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', ws => {
-  console.log('Client connected');
+wss.on("connection", (ws) => {
+  console.log("Client connected");
 
-  ws.on('message', async message => {
+  ws.on("message", async (message) => {
     const data = JSON.parse(message);
-    console.log('Received from client:', data);
+    console.log("Received from client:", data);
 
     // Handle client commands, e.g., switch scenes
-    if (data.command === 'switchScene') {
-      await obs.call('SetCurrentProgramScene', { sceneName: data.sceneName });
+    if (data.command === "switchScene") {
+      console.log(`Switching to scene: ${data.sceneName}`);
+      await obs.call("SetCurrentProgramScene", { sceneName: data.sceneName });
     }
   });
 
   // Forward OBS events to the client
-  obs.on('CurrentProgramSceneChanged', eventData => {
-    ws.send(JSON.stringify({ event: 'sceneChanged', sceneName: eventData.sceneName }));
+  obs.on("CurrentProgramSceneChanged", (eventData) => {
+    ws.send(
+      JSON.stringify({ event: "sceneChanged", sceneName: eventData.sceneName })
+    );
   });
 
-  ws.on('close', () => {
-    console.log('Client disconnected');
+  ws.on("close", () => {
+    console.log("Client disconnected");
   });
 });
 
 server.listen(8080, () => {
-  console.log('Server is listening on http://localhost:8080');
+  console.log("Server is listening on http://localhost:8080");
 });
